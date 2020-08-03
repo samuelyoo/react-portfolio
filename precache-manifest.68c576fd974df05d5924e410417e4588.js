cfca02a8a3dffa412096cff661e6597de3ad5c24
kuro sammy <sam.hc.yoo@gmail.com>
2020-08-03 02:21:55 -0400

    Updates

---
+++ precache-manifest.68c576fd974df05d5924e410417e4588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9bd439e049a289d0784147877eaa32",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "778136fd8509b8985e18",
+    "url": "/react-portfolio/static/css/main.9ea3fe66.chunk.css"
+  },
+  {
+    "revision": "e59e62b36a57890c2924",
+    "url": "/react-portfolio/static/js/2.8b092c24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.8b092c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778136fd8509b8985e18",
+    "url": "/react-portfolio/static/js/main.39b28ba8.chunk.js"
+  },
+  {
+    "revision": "a0fa0e19f9820f5e052e",
+    "url": "/react-portfolio/static/js/runtime-main.a4c7fc69.js"
+  }
+]);
